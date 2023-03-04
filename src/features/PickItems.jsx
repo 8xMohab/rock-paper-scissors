9699b4bf6cf04e88221b1bb65b785b68dec06286
8xMohab <0dem411@gmail.com>
2023-03-04 14:37:34 +0200

    fixing triangle bg url

--- src/features/PickItems.jsx
+++ src/features/PickItems.jsx
@@ -3,12 +3,9 @@ import { Items } from "../GameLogic";
 import Item from "./item/Item";
 function PickItems() {
   return (
-    <div className="w-52 h-52 lg:w-64 lg:h-64 mx-auto mt-48 bg-triangle bg-no-repeat bg-contain relative">
+    <div className="w-52 h-52 lg:w-64 lg:h-64 mx-auto mt-48 bg-[url('/bg-triangle.svg')] bg-no-repeat bg-contain relative">
       {Items.map((item) => (
-        <Item
-          key={item.name}
-          item={item}
-        />
+        <Item key={item.name} item={item} />
       ))}
     </div>
   );
